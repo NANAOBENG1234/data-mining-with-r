7a34e601067001c17f1a1b060ba85f4ecf1bc3f4
Nana Obeng Benedict <bnoagyemang@st.ug.edu.gh>
2026-08-05 16:27:56 +0000

    Working on hypothesis testting

--- STAT240 Assignment/data-mining with r/Untitled.R
+++ STAT240 Assignment/data-mining with r/Untitled.R
@@ -16,12 +16,12 @@ print(data(x,y))
       
       
 Spearman<-function(x,y){
-n =length(x)
-numerator= n*(sum(x*y))- (sum(x)*sum(y))
-d1<-c(n*(sum(x^2)-(sum(x)^2))
-s1<-c(n*(sum(y^2)-(sum(y)^2))
-denominator=(d1*s1)^0.5
- spear= numerator/(denominator)
+n =length(x),
+numerator= n*(sum(x*y))- (sum(x)*sum(y)),
+d1<-c(n*(sum(x^2)-(sum(x)^2) ),
+s1<-c(n*(sum(y^2)-(sum(y)^2)),
+denominator=(d1*s1)^0.5,
+ spear= numerator/(denominator),
  return(spear)
 }
  print (Spearman(x,y))
@@ -53,14 +53,14 @@ denominator=(d1*s1)^0.5
  
  
  ##While loops 
- i<-10
+ i<-10,
  while(i>0){
    print("hello")
    i<-i-1
  }
  
  
- x<-6
+ x<-6,
  if(x>0){
    print("X is positive")
  }else{
@@ -81,11 +81,11 @@ denominator=(d1*s1)^0.5
  }else{
    print("zero")
  }
- 
+ ##====Work on this =======
  x<-c(2,4,7,10,20,22,23)
  y<-c(2,7,9,10,10,20,24)
  
- data<-function(x,y){
+ data<-function(x,y){ 
    d=rank(x)-rank(y)
    numerator<-c(6*sum(d^2))
    n = length(x) 
@@ -94,14 +94,35 @@ denominator=(d1*s1)^0.5
    return (Corr)
  Corr.cat<-function(x,y){
  
- if(corr<=0.001 & corr <=0.3){
+ if(corr<=0 & corr <=0.3){
    print("Weak correlation")
  }elseif(corr<=0.31 & corr <=0.69){
    print("Moderate Correlation")
  }esle{
    print("Strong correlation")
+      }
  }
- }
- print(data(x,y))
+ print(data)
+ 
+ 
+ 
+ 
+ 
+ #===============Hypothesis Testing ===========
+ 
+ ?t.test
+  library(MASS)
+names(cabbages)
+?cabbages
+ summary(cabbages$HeadWt)
+ 
+ ##==== HO: mu = 2.84Kg
+ ## ======H1 : mu < 2.84 Kg
+ 
+t.test(cabbages$HeadWt, alternative = "less", 
+      mu = 2.84) 
+ 
+ 
+ 
  
  
