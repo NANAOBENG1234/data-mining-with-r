f1861e5f9f6b7b56673d402438944fcfa09bbd9e
Obeng Benedict Agyeman <nanaobengagyeman752@gmail.com>
2026-08-05 02:07:20 +0000

    WOrking on the assignment / Summary on a data

---
+++ STAT240 Assignment/data-mining with r/Assignment3.R
@@ -0,0 +1,23 @@
+#=============QUESTION ONE =======================
+
+
+Survey <- c(21,25,18,30,29,22,24,27,20,23)
+
+summary_stat <- function(x){
+  result = list(
+    n = length(x),
+    Mean  = mean(x),
+    Median = median(x),
+    Maxmimum = max(x),
+    Mininmum = min(x),
+    Varaince = var(x),
+    sd = sd(x),
+    range = max(x)-min(x)
+    
+    
+  )
+  return(result)
+}
+
+summary_stat(Survey)
+
